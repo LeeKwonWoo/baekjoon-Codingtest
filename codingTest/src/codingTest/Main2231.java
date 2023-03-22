65e321436e7057a4ef4bc2039ea41eb121d9726f
LeeKwonWoo <kenu12@naver.com>
2023-03-22 13:44:24 +0900

    No 2231

--- codingTest/src/codingTest/Main2231.java
+++ codingTest/src/codingTest/Main2231.java
@@ -6,26 +6,41 @@ public class Main2231 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int sum = 0, result = 0, mid = 0;
-		for (int i = 1; i < 10; i++) {
-			for (int j = 1; j < 10; j++) {
-				for (int k = 1; k < 10; k++) {
-					sum = i + k + j;
-					String s1 = Integer.toString(i);
-					String s2 = Integer.toString(j);
-					String s3 = Integer.toString(k);
-					String m = s1 + s2 + s3;
-					mid = Integer.valueOf(m);
-					result = n - mid;
-					if (result == sum) {
-						System.out.print(mid);
-					}
-				}
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			int number = i;
+			int sum = 0;
+			while (number!=0) {
+				sum += number % 10;
+				number/=10;
 			}
-			if (i==9) {
-				mid = 0;
-				System.out.println(mid);
+
+			if (sum+i == n) {
+				result = i;
+				break;
 			}
 		}
+		System.out.println(result);
+//		int sum = 0, result = 0, mid = 0;
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 1; j < 10; j++) {
+//				for (int k = 1; k < 10; k++) {
+//					sum = i + k + j;
+//					String s1 = Integer.toString(i);
+//					String s2 = Integer.toString(j);
+//					String s3 = Integer.toString(k);
+//					String m = s1 + s2 + s3;
+//					mid = Integer.valueOf(m);
+//					result = n - mid;
+//					if (result == sum) {
+//						System.out.print(mid);
+//					}
+//				}
+//			}
+//			if (i==9) {
+//				mid = 0;
+//				System.out.println(mid);
+//			}
+//		}
 	}
 }
