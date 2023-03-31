d5cfd358b9972362ea64cc41b3e9ead995e9b767
LeeKwonWoo <kenu12@naver.com>
2023-03-31 14:54:15 +0900

    No 2587

--- codingTest/src/codingTest/Main2587.java
+++ codingTest/src/codingTest/Main2587.java
@@ -1,43 +1,7 @@
 package codingTest;
 import java.util.Scanner;
 public class Main2587 {
-//	int[] sorted = new int[5];
-//	void merge(int[] arr, int left, int mid, int right) {
-//		int i, j, k, l;
-//		i = left;
-//		j = mid+1;
-//		k = left;
-//		while (i <= mid && j <= right) {
-//			if (arr[i]<=arr[j]) {
-//				sorted[k++] = arr[i++];
-//			} else {
-//				sorted[k++] = arr[j++];
-//			}
-//		}
-//		if (i > mid) {
-//			for (l = j; l < right; l++) {
-//				sorted[k++] = arr[l];
-//			}
-//		} else {
-//			for (l = j; l < mid; l++) {
-//				sorted[k++] = arr[l];
-//			}
-//		}
-//		for(l = left; l <= right; l++) {
-//			arr[l] = sorted[l];
-//		}
-//	}
-//	
-//	void merge_sort(int[] arr, int left, int right) {
-//		int mid;
-//		if (left < right) {
-//			mid = (left+right)/2;
-//			merge_sort(arr, left, mid);
-//			merge_sort(arr, mid+1, right);
-//			merge(arr,left, mid, right);
-//		}
-//	}
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n1 = scan.nextInt();
@@ -46,9 +10,7 @@ public class Main2587 {
 		int n4 = scan.nextInt();
 		int n5 = scan.nextInt();
 		int[] arr = {n1, n2, n3, n4, n5};
-//		new Main2587().merge_sort(arr, 0, arr.length-1);
 		int avg = (n1+n2+n3+n4+n5)/5;
-				
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				if (arr[i] < arr[j]) {
@@ -59,9 +21,44 @@ public class Main2587 {
 				}
 			}
 		}
-		
 		System.out.println(avg);
 		System.out.println(arr[2]);
 	}
 
 }
+//int[] sorted = new int[5];
+//void merge(int[] arr, int left, int mid, int right) {
+//	int i, j, k, l;
+//	i = left;
+//	j = mid+1;
+//	k = left;
+//	while (i <= mid && j <= right) {
+//		if (arr[i]<=arr[j]) {
+//			sorted[k++] = arr[i++];
+//		} else {
+//			sorted[k++] = arr[j++];
+//		}
+//	}
+//	if (i > mid) {
+//		for (l = j; l < right; l++) {
+//			sorted[k++] = arr[l];
+//		}
+//	} else {
+//		for (l = j; l < mid; l++) {
+//			sorted[k++] = arr[l];
+//		}
+//	}
+//	for(l = left; l <= right; l++) {
+//		arr[l] = sorted[l];
+//	}
+//}
+//
+//void merge_sort(int[] arr, int left, int right) {
+//	int mid;
+//	if (left < right) {
+//		mid = (left+right)/2;
+//		merge_sort(arr, left, mid);
+//		merge_sort(arr, mid+1, right);
+//		merge(arr,left, mid, right);
+//	}
+//}
